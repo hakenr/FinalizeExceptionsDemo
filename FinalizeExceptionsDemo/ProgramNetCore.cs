edc18be8672eeccb2a11eebab5be5b50093d8b40
Haken, Robert <haken@havit.cz>
2019-04-27 15:15:01 +0200

    Add project files.

---
+++ FinalizeExceptionsDemo/ProgramNetCore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FinalizeExceptionsDemoNetCore
+{
+	public class ProgramNetCore
+	{
+		public static void Main(string[] args)
+		{
+			AllocateFinalizableObject();
+
+			Console.WriteLine("Let's do the GC!");
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			Console.WriteLine("Still alive!");
+			Console.Read();
+		}
+
+		private static void AllocateFinalizableObject()	// needed for .NET Core where the instance is still held in regs in debug
+		{
+			var x = new MyClass(1);
+			x = new MyClass(2);
+		}
+	}
+
+	public class MyClass
+	{
+		private readonly int id;
+
+		public MyClass(int id)
+		{
+			this.id = id;
+		}
+
+		~MyClass()
+		{
+			Console.WriteLine($"Hello from Finalize:{id}...");
+
+			MethodWithException();
+
+			//throw new Exception($"ExceptionFromFinalize:{id}");
+		}
+
+		private void MethodWithException()
+		{
+			throw new Exception($"ExceptionFromMethodCalledFromFinalize:{id}");
+		}
+	}
+}
---
+++ FinalizeExceptionsDemoNetFramework/ProgramNetFramework.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizeExceptionsDemoNetFramework
+{
+	public class ProgramNetFramework
+	{
+		public static void Main(string[] args)
+		{
+			AllocateFinalizableObject();
+
+			Console.WriteLine("Let's do the GC!");
+
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+
+			Console.WriteLine("Still alive!");
+			Console.Read();
+		}
+
+		private static void AllocateFinalizableObject() // needed for .NET Core where the instance is still held in regs in debug
+		{
+			var x = new MyClass(1);
+			x = new MyClass(2);
+		}
+	}
+
+	public class MyClass
+	{
+		private readonly int id;
+
+		public MyClass(int id)
+		{
+			this.id = id;
+		}
+
+		~MyClass()
+		{
+			Console.WriteLine($"Hello from Finalize:{id}...");
+
+			MethodWithException();
+
+			throw new Exception($"ExceptionFromFinalize:{id}");
+		}
+
+		private void MethodWithException()
+		{
+			throw new Exception($"ExceptionFromMethodCalledFromFinalize:{id}");
+		}
+	}
+}
